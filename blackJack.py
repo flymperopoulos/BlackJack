79079a3b11e896fc2b39b38d9495cc983c53f76e
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-05-08 19:41:23 -0400

    Added deck creation option

--- blackJack.py
+++ blackJack.py
@@ -1,1 +1,27 @@
-import random
+def createDeck(numberOfDecks):
+	# Create an empty newDeck
+	newDeck = []
+
+	# Define a list of suits and a dictionary of card values
+	cardTypes = ['Clubs','Spades','Diamonds','Hearts']
+	cardNumbers = {'Ace':1, '2':2, '3':3, '4':4, '5':5, '6':6,'7':7,'8':8, '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10}
+
+	# Number of piles initialized
+	numberofPiles = 0
+
+	# Append cardTypes and cardNumbers accordingly
+	while numberofPiles < numberOfDecks:
+		for suit in cardTypes:
+			newL = []
+			for key in cardNumbers:
+				newL.append([suit,[key,cardNumbers[key]]])
+			newDeck.append(newL)
+		numberofPiles += 1
+
+	# Determines the number of decks used
+	print "The amount of decks used equals " + str(len(newDeck)/4) + "."
+
+	# Return the final Deck	
+	return newDeck
+
+print createDeck(1)
